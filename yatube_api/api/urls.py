6311ba686a90510106ef2629b1332bc72ca04e87
stupak-alina <medlin05@mail.ru>
2025-05-21 16:40:59 +0300

    change urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from .views import PostViewSet, CommentViewSet, FollowViewSet, GroupViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register("v1/posts", PostViewSet)
+router.register(r"v1/posts/(?P<post_id>\d+)/comments", CommentViewSet)
+router.register("v1/follow", FollowViewSet)
+router.register("v1/groups", GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
